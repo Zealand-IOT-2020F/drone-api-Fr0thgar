5b920457064159a2cd75a3cafb819ce220d9b20a
JonasBSaxild <43033098+Fr0thgar@users.noreply.github.com>
2020-03-20 11:48:10 +0100

    Update Drone, Main

--- Drone.py
+++ Drone.py
@@ -2,9 +2,19 @@ import socket
 import sys
 import time
 
+#region Commands
+#endregion
 
 class Drone(object):
-    """description of class"""
+    """ In this class der wil be 3 regions with commands and information that the drone will have. 
+        The 3 regions are StartUp, Commands and information messages. 
+        The start region includes det connection to the wifi and drone and the functions: sendMessage and printInfo.
+        The commands includes the commands that the drone can perform.
+        The information messages includes the messages about battery, flytime, serial number and SDK informations."""
+    
+#region Startup
+
+    #Constructor 
     def __init__(self, ip, port):
         self.TelloIp = ip
         print("ip: " + ip)
@@ -15,7 +25,13 @@ class Drone(object):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.tello_address = ("192.168.10.1", 8889)
         self.sock.bind(self.locaddr)
-
+    
+    #Connection
+    def connect(self,wait):
+        print("Connect")
+        result = self.sendMessage("command")
+        print(result)
+        time.sleep(wait)
 
     #Sending messages/commands to the drone
     def sendMessage(self, TelloMessage):
@@ -27,44 +43,128 @@ class Drone(object):
         
         return "from sendmessage " + TelloMessage + " end "
         
-    #Print out information abbut location
-    def printInfo(self):
+    #Print out information about location
+    def printInfo(self,wait):
         print("Hello Drone at : " + self.TelloIp)
-
-    #Connection
-    def connect(self):
-        print("Connect")
-        result = self.sendMessage("command")
-        print(result)
+        time.sleep(wait)    
+    
+#endregion
+#region Commands
 
     #TakeOff the ground
-    def takeOff(self):
+    def takeOff(self,wait):
         print("takeOff")
         result = self.sendMessage("TakeOff")
+        time.sleep(wait)
 
     #Landing the drone
-    def land(self):
+    def land(self,wait):
         print("land")
         result = self.sendMessage("land")
-    
+        time.sleep(wait)
+     
     #End the drone
-    def end(self):
+    def end(self,wait):
         print("end")
         self.sock.close()
+        time.sleep(wait)
 
     #Turn clockwise
-    def cw(self,x):
+    def cw(self,x,wait):
         print("cw")
         result = self.sendMessage("cw " + x)
+        time.sleep(wait)
     
     #Turn counter clockwise
-    def ccw(self,x):
+    def ccw(self,x,wait):
         print("ccw")
         result = self.sendMessage("ccw " + x)
+        time.sleep(wait)
 
-    #Give a battery message
-    def battery(self):
-        result = self.sendMessage("battery?")
-        return result
+    #Fly up
+    def up(self, x, wait):
+        print("up")
+        result = self.sendMessage("up " + x)
+        time.sleep(wait)
+    
+    #Fly down
+    def down(self, x, wait):
+        print("down")
+        result = self.sendMessage("down " + x)
+        time.sleep(wait)
+    
+    #Turn left
+    def left(self, x, wait):
+        print("left")
+        result = self.sendMessage("left " + x)
+        time.sleep(wait)
+    
+    #Turn right
+    def right(self, x, wait):
+        print("left")
+        result = self.sendMessage("left " + x)
+        time.sleep(wait)
+
+    #Move forward
+    def forward(self, x, wait):
+        print("forward")
+        result = self.sendMessage("forward " + x)
+        time.sleep(wait)
+
+    #Move backwards
+    def back(self, x, wait):
+        print("back")
+        result = self.sendMessage("back " + x)
+        time.sleep(wait)
+        
+    #Flip in given direction
+    def flip(self, x, wait):
+        print("flip")
+        result = self.sendMessage("flip " + x)
+        time.sleep(wait)
+    
+    #Hover
+    def stop(self, wait):
+        print("stop")
+        result = self.sendMessage("stop")
+        time.sleep(wait)
+
+
+    #GoXYZSpeed
+    def goXYZSpeed(self, x, y, z, speed, wait):
+        print("goXYZSpeed")
+        result = self.sendMessage("go " + x + " " + y + " " + z + " " + speed)
+        time.sleep(wait)
 
     
+    
+
+#endregion
+
+#region informations messages
+
+    #Give a battery message
+    def battery(self,wait):
+        result = self.sendMessage("battery?")
+        time.sleep(wait)
+        return result
+    
+    #Flytime
+    def time(self, wait):
+        result = self.sendMessage("time?")
+        time.sleep(wait)
+        return result
+    
+    #Trello SDK version 
+    def sdk(self, wait):
+        result = self.sendMessage("sdk?")
+        time.sleep(wait)
+        return result
+    
+    #Trello serial number
+    def sn(self, wait):
+        result = self.sendMessage("sn?")
+        time.sleep(wait)
+        return result
+        
+#endregion
--- Main.py
+++ Main.py
@@ -6,37 +6,32 @@ import time
 #drone_1 = drone.Drone("128.1.1.1", 8889)
 
 # Test drone
+#drone start up
 drone_1 = drone.Drone('192.168.1.1', 8889)
-
-
 drone_1.printinfo()
-
 drone_1.connect()
 
-print(drone_1.battery())
-time.sleep(1)
-print(drone_1.time())
-time.sleep(1)
-print(drone_1.sdk())
-time.sleep(1)
-print(drone_1.sn())
+#Print out start information
+print(drone_1.battery(0.1))
+print(drone_1.time(0.1))
+print(drone_1.sdk(0.1))
+print(drone_1.sn(0.1))
 
-drone_1.takeOff()
-time.sleep(1)
-drone_1.cw("90")
-time.sleep(1)
-drone_1.ccw("360")
-drone_1.cw("180")
-time.sleep(1)
-print(drone_1.battery())
-time.sleep(1)
-print(drone_1.time())
-time.sleep(1)
-print(drone_1.sdk())
-time.sleep(1)
-print(drone_1.sn())
+#Drone flightplan 
+drone_1.takeOff(1)
+drone_1.cw("90",1)
+drone_1.ccw("360",1)
+drone_1.cw("180",1)
+drone_1.up("60",1)
+drone_1.flip("b",1)
+drone_1.down("30",1)
+drone_1.left("20",1)
+drone_1.right("20",1)
 
+#print out landing information
+print(drone_1.battery(0.1))
+print(drone_1.time(0.1))
 
-drone_1.land()
-
+#Landing sequence
+drone_1.land(1)
 drone_1.end()
