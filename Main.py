464a04bbcb527252b7885708755fdcd83a3181cd
JonasBSaxild <43033098+Fr0thgar@users.noreply.github.com>
2020-03-20 13:33:27 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,11 +3,11 @@ import sys
 import time
 
 # Live drone
-drone_1 = drone.Drone("128.1.1.1", 8889)
+#drone_1 = drone.Drone("128.1.1.1", 8889)
 
 # Test drone
 # drone start up
-#drone_1 = drone.Drone('192.168.1.1', 8889)
+drone_1 = drone.Drone('192.168.10.1', 8889)
 drone_1.printInfo(1)
 drone_1.connect(1)
 
