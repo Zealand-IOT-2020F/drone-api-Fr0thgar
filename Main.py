c92af1bf7ba525a560b1ce817f37ce550f60e1c1
JonasBSaxild <43033098+Fr0thgar@users.noreply.github.com>
2020-03-20 10:51:24 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,23 @@
+import drone
+import sys
+import time
+
+drone_1 = drone.Drone("128.1.1.1", 8889)
 
 #here you should interact with the drone
+
+drone_1.printinfo()
+
+drone_1.connect()
+
+drone_1.takeOff()
+time.sleep(1)
+drone_1.cw("90")
+time.sleep(1)
+drone_1.ccw("360")
+drone_1.cw("180")
+time.sleep(1)
+print(drone_1.battery())
+drone_1.land()
+
+drone_1.end()
