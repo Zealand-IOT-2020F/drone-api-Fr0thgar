c97d19abb6b4c82b4d74f9869acf49cce5bcd417
JonasBSaxild <43033098+Fr0thgar@users.noreply.github.com>
2020-03-20 11:09:02 +0100

    Main og Drone updated

--- Drone.py
+++ Drone.py
@@ -5,10 +5,66 @@ import time
 
 class Drone(object):
     """description of class"""
+    def __init__(self, ip, port):
+        self.TelloIp = ip
+        print("ip: " + ip)
+        self.TelloPort = port
+        self.Host = ""
+        self.HostPort = 9000
+        self.locaddr = (self.Host, self.HostPort)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.tello_address = ("192.168.10.1", 8889)
+        self.sock.bind(self.locaddr)
 
 
+    #Sending messages/commands to the drone
+    def sendMessage(self, TelloMessage):
+        print("TelloMessage "+ TelloMessage +" end")
+        msg = TelloMessage.encode(encoding="utf-8")
+        sent  = self.sock.sendto(msg, self.tello_address)
+        data, server = self.sock.recvfrom(1518)
+        print(data.decode(encoding="utf-8"))
+        
+        return "from sendmessage " + TelloMessage + " end "
+        
+    #Print out information abbut location
+    def printInfo(self):
+        print("Hello Drone at : " + self.TelloIp)
+
+    #Connection
+    def connect(self):
+        print("Connect")
+        result = self.sendMessage("command")
+        print(result)
+
+    #TakeOff the ground
     def takeOff(self):
         print("takeOff")
-        #add the takeoff code
+        result = self.sendMessage("TakeOff")
+
+    #Landing the drone
+    def land(self):
+        print("land")
+        result = self.sendMessage("land")
+    
+    #End the drone
+    def end(self):
+        print("end")
+        self.sock.close()
+
+    #Turn clockwise
+    def cw(self,x):
+        print("cw")
+        result = self.sendMessage("cw " + x)
+    
+    #Turn counter clockwise
+    def ccw(self,x):
+        print("ccw")
+        result = self.sendMessage("ccw " + x)
+
+    #Give a battery message
+    def battery(self):
+        result = self.sendMessage("battery?")
+        return result
 
     
--- Main.py
+++ Main.py
@@ -2,13 +2,24 @@ import drone
 import sys
 import time
 
-drone_1 = drone.Drone("128.1.1.1", 8889)
+# Live drone
+#drone_1 = drone.Drone("128.1.1.1", 8889)
 
-#here you should interact with the drone
+# Test drone
+drone_1 = drone.Drone('192.168.1.1', 8889)
+
 
 drone_1.printinfo()
 
 drone_1.connect()
+
+print(drone_1.battery())
+time.sleep(1)
+print(drone_1.time())
+time.sleep(1)
+print(drone_1.sdk())
+time.sleep(1)
+print(drone_1.sn())
 
 drone_1.takeOff()
 time.sleep(1)
@@ -18,6 +29,14 @@ drone_1.ccw("360")
 drone_1.cw("180")
 time.sleep(1)
 print(drone_1.battery())
+time.sleep(1)
+print(drone_1.time())
+time.sleep(1)
+print(drone_1.sdk())
+time.sleep(1)
+print(drone_1.sn())
+
+
 drone_1.land()
 
 drone_1.end()
