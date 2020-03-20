968193a2a4cc07e9272adf05427769fcf0316fb9
JonasBSaxild <43033098+Fr0thgar@users.noreply.github.com>
2020-03-20 13:30:54 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,8 +8,8 @@ drone_1 = drone.Drone("128.1.1.1", 8889)
 # Test drone
 # drone start up
 #drone_1 = drone.Drone('192.168.1.1', 8889)
-drone_1.printinfo()
-drone_1.connect()
+drone_1.printInfo(1)
+drone_1.connect(1)
 
 # Print out start information
 print(drone_1.battery(0.1))
@@ -23,7 +23,7 @@ drone_1.cw("90", 1)
 drone_1.ccw("360", 1)
 drone_1.cw("180", 1)
 drone_1.up("60", 1)
-drone_1.flip("b", 1)
+#drone_1.flip("b", 1)
 drone_1.down("30", 1)
 drone_1.left("20", 1)
 drone_1.right("20", 1)
@@ -34,4 +34,4 @@ print(drone_1.time(0.1))
 
 # Landing sequence
 drone_1.land(1)
-drone_1.end()
+drone_1.end(1)
